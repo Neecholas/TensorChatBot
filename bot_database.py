3a3803290c93e0b0b03db93c6a7fedc8d774d696
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-16 18:54:40 +0100

    Coded the first steps of the test database using SQL lite

--- bot_database.py
+++ bot_database.py
@@ -1,3 +1,18 @@
 import sqlite3
 import json
 from datetime import datetime
+
+timeframe = '2015-05'
+sql_transaction = []
+
+connection = sqlite3.connect('{}.db'.format(timeframe))
+c = connection.cursor()
+
+def create_table():
+  c.execute("""CREATE TABLE IF NOT EXISTS parent_reply(
+      parent_id TEXT PRIMARY KEY, comment.id TEXT UNIQUE,
+      parent TEXT, comment TEXT, subreddit TEXT, unit INT, score INT
+      )""")
+
+if __name__ == "__main__":
+  create_table()
