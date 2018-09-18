9eb66fb0a9b9d29b5ee3f2c36c635836227128e8
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-18 13:19:12 +0100

    Began to make the algorithm to get the data from the downloaded files

--- bot_database.py
+++ bot_database.py
@@ -25,3 +25,19 @@ def create_table():
 
 if __name__ == "__main__":
   create_table()
+  row_counter = 0
+  paired_rows = 0
+  with open("C:/Users/right/Downloads/Chatbot_data/reddit_data/{}/RC_{}".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+  #opens the file and takes the timeframe by splitting the initial timeframe string
+  #and taking the first part (array index 0)
+  #then takes the full timeframe and has it as the second variable
+    for row in f:
+      row_counter += 1
+      row = json.loads(row)
+      parent_id = row['parent_id']
+      body = format_data(row['body'])
+      created_utc = row['created_utc']
+      score = row['score']
+      subreddit = row['subreddit']
+
+
