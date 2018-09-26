73af3c2b0bb01b672fad168cf81b2fd65d105913
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-26 22:57:52 +0100

    Created the start of the setup

--- create_training_data.py
+++ create_training_data.py
@@ -1,4 +1,16 @@
 import sqlite3
 import pandas as pd
 
-timeframes = ['2015-05']
+timeframes = ['2015-02']
+
+for timeframe in timeframes:
+  connection = sqlite3.connect('{}.db'.format(timeframe))
+  c = connecition.cursor()
+  #creates the connection cursor
+  limit = 5000
+  last_unit = 0
+  cur_length = limit
+  counter = 0
+  test_done = False
+  while cur_length == limit:
+    df = pd.read_sql("SELECT * FROM parent_reply WHERE unit > {} AND parent NOT NULL AND score > 0 ORDER BY unit ASC LIMIT {}".format(last_unit, limit), conneciton)
