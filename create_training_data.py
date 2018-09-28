93b7200cb79b0f7441f3e85f9b750f3514c9b624
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-28 18:13:34 +0100

    Documented the code

--- create_training_data.py
+++ create_training_data.py
@@ -11,13 +11,16 @@ for timeframe in timeframes:
   c = connection.cursor()
   #creates the connection cursor
   limit = 5000
+  #the amount we pull at once to put into the pandas dataframe
   last_unit = 0
   cur_length = limit
   counter = 0
   test_done = False
   while cur_length == limit:
     df = pd.read_sql("SELECT * FROM parent_reply WHERE unit > {} AND parent NOT NULL AND score > 0 ORDER BY unit ASC LIMIT {}".format(last_unit, limit), connection)
+    #grabs the first 5000 replys (or however many we want from the database) greater than 0 or the last unit of the last batch.
     last_unit = df.tail(1)['unit'].values[0]
+    #sets the time to the last time checked, allowing us to pull the next 5000 afterwards
     cur_length = len(df)
     if not test_done:
       with open("../data_bases/test.from", 'a', encoding = 'utf8') as f:
